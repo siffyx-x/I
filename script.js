1031c66e8124fb611001d9d8de235633a850d60d
siffyx-x <chill.isz622@gmail.com>
2025-06-11 21:13:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // 「はい」ボタンがクリックされた時の処理
     confirmButton.addEventListener('click', () => {
         // の紹介ページ (.html) に直接遷移
-        const nextPageUrl = ‘iw.html';
+        const nextPageUrl = 'iw.html';
         window.location.href = nextPageUrl;
     });
 
